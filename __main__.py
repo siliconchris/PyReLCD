5603313447f98d93db3c4a5b1d59c1e3f3d1e1c9
Christian Guenther <chris.guenther@mac.com>
2015-07-30 23:03:21 +0200

    added bundling functionality and put LCD in it's own class file

--- __main__.py
+++ __main__.py
@@ -1,73 +1,17 @@
 __author__ = '@siliconchris'
 
-
-from flask import json
-from flask import request
 from flask import Flask
-from flask import render_template
-
 import argparse
 import sys
+
+from hd44780_class import HD44780
+from rest_writer_class import rest_writer
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def api_root():
-    return render_template('info.html')
-
-@app.route('/messageform')
-def api_form():
-    return render_template('messageform.html')
-
-@app.route('/write', methods = ['POST'])
-def api_message():
-    if request.headers['Content-Type'] == 'text/plain':
-        writetext = request.data
-        return "Text Message: " + request.data
-    elif request.headers['Content-Type'] == 'application/json':
-        try:
-            data = json.loads(json.dumps(request.json))
-            row1 = data['row1']
-            row2 = data['row2']
-            if app.debug:
-                print 'row1: ' + row1
-                print 'row2: ' + row2
-
-            writetext = row1 + '\\n' + row2
-        except:
-            if app.debug:
-                print "Error parsing the json:", sys.exc_info()[0]
-            else:
-                print "Error parsing the json"
-
-        return "JSON Message: " + json.dumps(request.json)
-    elif request.form['text']:
-        try:
-            row1 = request.form['row1']
-            row2 = request.form['row2']
-            if app.debug:
-                print 'row1: ' + row1
-                print 'row2: ' + row2
-
-            writetext = row1 + '\\n' + row2
-        except:
-            if app.debug:
-                print "Error parsing the json:", sys.exc_info()[0]
-            else:
-                print "Error parsing the json"
-
-        return "Form Message: " + request.data
-    else:
-        return "415 Unsupported Media Type ;)"
-
-    if app.debug:
-        print(writetext)
-    # now write received text to the LCD
-    #lcd.message(writetext)
-
-
 if __name__ == '__main__':
+    lcd = HD44780()
     app.debug = True
 
     inetaddr = '0.0.0.0'
@@ -75,9 +19,6 @@ if __name__ == '__main__':
     security_enable = False
     security_token = ''
     security_token_key = ''
-    row1 = ''
-    row2 = ''
-    txtmessage = ''
 
     parser = argparse.ArgumentParser(description='Write a message on a 16x2 LCD. The service waits for the text to display to be passed at the endpoint /write/')
     parser.add_argument('-f', '--foreground', action='store_true', dest="demonize", default=False,
@@ -109,7 +50,7 @@ if __name__ == '__main__':
         security_enable = args.sec_enable
 
     try:
-        app.run(inetaddr, inetport)
+        rest_writer.run(inetaddr, inetport)
     except:
         if app.debug:
             print "Unexpected error:", sys.exc_info()[0]
